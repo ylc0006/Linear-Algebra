8b1e99004611a101885990f943257fe3c68678da
Yan-lin Chen <ylc0006@gmail.com>
2017-08-31 16:41:07 +0800

    Add files via upload

--- linsys.py
+++ linsys.py
@@ -1,7 +1,6 @@
 from decimal import Decimal, getcontext
 from copy import deepcopy
 
-from vector import Vector
 from plane import Plane
 
 getcontext().prec = 30
@@ -85,20 +84,25 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-p0 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+
+# code for playing 
+p0 = Plane([1, 1, 1], 1)
+p1 = Plane([0, 1, 0], 2)
+p2 = Plane([1, 1, -1], constant_term=3)
+p3 = Plane([1, 0, -2], constant_term=2)
 
 s = LinearSystem([p0,p1,p2,p3])
 
-print s.indices_of_first_nonzero_terms_in_each_row()
-print '{},{},{},{}'.format(s[0],s[1],s[2],s[3])
-print len(s)
-print s
+
+
+
+print (s.indices_of_first_nonzero_terms_in_each_row())
+print ('{},{},{},{}'.format(s[0],s[1],s[2],s[3]))
+print (len(s))
+print (s)
 
 s[0] = p1
-print s
+print (s)
 
-print MyDecimal('1e-9').is_near_zero()
-print MyDecimal('1e-11').is_near_zero()
+print (MyDecimal('1e-9').is_near_zero())
+print (MyDecimal('1e-11').is_near_zero())
