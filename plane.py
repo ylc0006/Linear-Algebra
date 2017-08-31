c8b5d7b4547681564f4df8c7ca025af884dd8d91
Yan-lin Chen <ylc0006@gmail.com>
2017-08-31 14:27:43 +0800

    Add files via upload

--- plane.py
+++ plane.py
@@ -15,13 +15,13 @@ class Plane(object):
         if not normal_vector:
             all_zeros = ['0']*self.dimension
             normal_vector = Vector(all_zeros)
-        self.normal_vector = normal_vector
+        self.normal_vector = normal_vector # initial to set a normal vector
 
         if not constant_term:
             constant_term = Decimal('0')
-        self.constant_term = Decimal(constant_term)
+        self.constant_term = Decimal(constant_term) # initial to set a value
 
-        self.set_basepoint()
+        self.set_basepoint() # initial to run a function
 
 
     def set_basepoint(self):
@@ -31,9 +31,9 @@ class Plane(object):
             basepoint_coords = ['0']*self.dimension
 
             initial_index = Plane.first_nonzero_index(n)
-            initial_coefficient = n[initial_index]
+            initial_coefficient = Decimal(n[initial_index])
 
-            basepoint_coords[initial_index] = c/initial_coefficient
+            basepoint_coords[initial_index] = c/initial_coefficient  # becasue c is decimal, so initial_coefficient needs to be decimal
             self.basepoint = Vector(basepoint_coords)
 
         except Exception as e:
@@ -84,9 +84,38 @@ class Plane(object):
         constant = round(self.constant_term, num_decimal_places)
         if constant % 1 == 0:
             constant = int(constant)
-        output += ' = {}'.format(constant)
+        output += ' = {}'.format(constant) # put value of constant into {}
 
         return output
+    # determine if 2 planes are paralle 
+    #(check if their normal vectors are parallel)
+    def is_paralle_to(self, plane):
+        return Vector(self.normal_vector).is_parallel_to(Vector(plane.normal_vector))
+       
+    # determine if 2 planes are equal 
+    #(the vector connecting one point on each plane is orthogonal to the plane's normal vector)
+    def __eq__(self, plane):
+       if Vector(self.normal_vector).is_zero():
+           if Vector(plane.normal_vector).is_zero():
+               return False
+           else:
+               diff = self.constant_term - plane.constant_term
+               return MyDecimal(diff).is_near_zero()
+       elif Vector(plane.normal_vector).is_zero():
+           return False
+       
+       if not self.is_paralle_to(plane):
+           return False
+        
+       p1= self.basepoint
+       p2= plane.basepoint
+       p2_p1_vector = p2.minus(p1)
+       if p2_p1_vector.orthogonal(Vector(self.normal_vector)):
+           return "same plane"
+       else:
+           return "not the same plane"
+    
+    
 
 
     @staticmethod
@@ -100,3 +129,17 @@ class Plane(object):
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+#Test cosde
+plane1 = Plane([-0.412, 3.806, 0.728], -3.46)
+plane2 = Plane([1.03, -9.515, -1.82], 8.65)
+plane3 = Plane([2.611, 5.528, 0.283], 4.6)
+plane4 = Plane([7.715, 8.306, 5.342], 3.76)
+plane5 = Plane([-7.926, 8.625, -7.212], -7.952)
+plane6 = Plane([-2.642, 2.875, -2.404], -2.443)
+
+print(plane1.is_paralle_to(plane2))
+print(plane3.is_paralle_to(plane4))
+print(plane5.is_paralle_to(plane6))
+print(plane1.__eq__(plane2))
+print(plane5.__eq__(plane6))
